f36311f737cc9b649ca55e3c4901821e4f53c28b
kawayayuuta <soonlabo@gmail.com>
2021-07-08 23:48:52 +0900

    回答

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-# coding=utf-8
-# これはサンプルの Python スクリプトです。
+# ABC200
+# A
 
-# ⌃R を押して実行するか、ご自身のコードに置き換えてください。
-# ⇧ を2回押す を押すと、クラス/ファイル/ツールウィンドウ/アクション/設定を検索します。
+import math
 
-
-def print_hi(name):
-    # スクリプトをデバッグするには以下のコード行でブレークポイントを使用してください。
-    print("Hi, {0}".format(name))  # ⌘F8を押すとブレークポイントを切り替えます。
-
-
-# ガター内の緑色のボタンを押すとスクリプトを実行します。
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# PyCharm のヘルプは https://www.jetbrains.com/help/pycharm/ を参照してください
+n = int(input())
+print(math.floor((n+99)/100))
